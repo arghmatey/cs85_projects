d9b8fbbf2f6f869e7f9d7e420eafd688a660b93c
Sarah <40401149+arghmatey@users.noreply.github.com>
2025-07-06 11:57:46 -0700

    Add validateEmail for validation and format check

--- ContactForm.php
+++ ContactForm.php
@@ -24,6 +24,26 @@
 
         return($retval); // Returns: cleaned up or empty value
     }
+
+    // Validates email input
+    function validateEmail($data, $fieldName) {
+        global $errorCount;
+                if (empty($data)) {
+            echo "\"$fieldName\" is a required field.<br /> \n";
+            ++$errorCount;
+            $retval = "";
+        }
+        else {
+            // Sanitize and validate email format
+            $retval = filter_var($data, FILTER_SANITIZE_EMAIL);
+
+            if (!filter_var($retval, FILTER_VALIDATE_EMAIL)) {
+                echo "\"$fieldName\" is not a valid e-mail address.<br />\n";
+            }
+        }
+
+        return($retval);
+    }
     ?>
 </body>
 </html>
