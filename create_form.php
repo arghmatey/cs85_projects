d09332e6f8562527b4031f844a7021c9caa737d9
Sarah <40401149+arghmatey@users.noreply.github.com>
2025-07-06 14:04:48 -0700

    Add for submissions logic and display results

--- create_form.php
+++ create_form.php
@@ -6,24 +6,56 @@
     <title>Secure Contact Form</title>
 </head>
 <body>
-    <form method="POST" action="">
-        <p>
-            <label for="fullName">Full Name:</label><br />
-            <input type="text" name="fullName" id="fullName" required>
-        </p>
-        <p>
-            <label for="email">Email Adress:</label><br />
-            <input type="email" name="email" id="email" required>
-        </p>
-        <p>
-            <label for="subject">Subject:</label><br />
-            <input type="text" name="subject" id="subject" required>
-        </p>
-        <p>
-            <label for="message">Message:</label><br />
-            <textarea name="message" id="message" rows="4" required></textarea>
-        </p>
-        <input type="submit" value="Send Message" />
-    </form>
+    <?php
+    function displayForm(){
+        ?>
+            <form method="POST" action="">
+                <p>
+                    <label for="fullName">Full Name:</label><br />
+                    <input type="text" name="fullName" id="fullName" required>
+                </p>
+                <p>
+                    <label for="email">Email Adress:</label><br />
+                    <input type="email" name="email" id="email" required>
+                </p>
+                <p>
+                    <label for="topic">Topic:</label><br />
+                    <input type="text" name="topic" id="topic" required>
+                </p>
+                <p>
+                    <label for="message">Message:</label><br />
+                    <textarea name="message" id="message" rows="4" required></textarea>
+                </p>
+                <input type="submit" value="Send Message" />
+            </form>
+        <?php
+    }
+
+    $fullName = "";
+    $email = "";
+    $topic = "";
+    $message = "";
+    $errorCount = 0;
+
+    function displayConfirmation($fullName, $email, $topic) {
+        echo "<p> Thank you, $fullName! We received your message about: \"$topic\"</p>";
+        echo "<p>We'll get back to you at $email.</p>"; 
+    }
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $fullName = htmlspecialchars($_POST['fullName']);
+        $email = htmlspecialchars($_POST['email']);
+        $topic = htmlspecialchars($_POST['topic']);
+        $message = htmlspecialchars($_POST['message']);
+
+        // Temporary raw results
+        displayConfirmation($fullName, $email, $topic);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST' || $errorCount > 0) {
+        displayForm();
+    }
+    ?>
+
 </body>
 </html>
