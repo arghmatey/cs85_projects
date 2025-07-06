10015b34027f7e14de8f879af33ebebb8b5d0d79
Sarah <40401149+arghmatey@users.noreply.github.com>
2025-07-06 12:04:16 -0700

    Add displayForm to render contact form

--- ContactForm.php
+++ ContactForm.php
@@ -44,6 +44,25 @@
 
         return($retval);
     }
+
+    // Displays contact form
+    function displayForm($Sender, $Email, $Subject, $Message) {
+        ?> <h2 style = "text-align:center">Contact Me</h2>
+        <form name="contact" action="ContactForm.php" method="post">
+            <p>Your Name:
+                <input type="text" name="Sender" value="<?php echo $Sender; ?>" /></p>
+            <p>Your E-mail:
+                <input type="text" name="Email" value="<?php echo $Email; ?>" /></p>
+            <p>Subject:
+                <input type="text" name="Subject" value="<?php echo $Subject; ?>" /></p>
+            <p>Message:
+                <input type="text" name="Message" value="<?php echo $Message; ?>" /></p>
+            <p>
+                <input type="reset" value="Clear Form" />&nbsp; $nbsp;
+                <input type="submit" name="Submit" value="Send Form" /></p>
+        </form>
+        <?php
+    }
     ?>
 </body>
 </html>
